3f6b6807485ccea6ea4d6cd58e29961019886433
Akanda Sina Kilicarslan <84199485+akandasina@users.noreply.github.com>
2026-04-26 00:28:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  base: "/Akanda_Portfolio/",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  vite: {
+    base: "/Akanda_Portfolio/",
   },
 });
